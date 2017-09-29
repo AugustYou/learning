c0fca0480cd0e2d37deafdff7ad9394d5ee39c7f
FelixXu <xufei@xiaoantimes.com>
2017-09-30 07:16:13 +0800

    del

--- dynamicdatasource/src/main/mybatis_gen/do_gen.bat
+++
@@ -1,2 +0,0 @@
-del /f /q /s gen
-java -jar mybatis-generator-core-1.3.2.jar -configfile mybatis_generator_config.xml
--- dynamicdatasource/src/main/mybatis_gen/do_gen.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-rm -rf gen/*
-java -jar mybatis-generator-core-1.3.2.jar -configfile mybatis_generator_config.xml
