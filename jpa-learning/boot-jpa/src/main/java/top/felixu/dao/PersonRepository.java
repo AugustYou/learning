63a225eab8289d9e4803e1a216a581f7d3326177
felixu <xufei_0320@163.com>
2017-11-28 23:12:03 +0800

    jpa learning

--- jpa-learning/boot-jpa/src/main/java/top/felixu/dao/PersonRepository.java
+++ jpa-learning/boot-jpa/src/main/java/top/felixu/dao/PersonRepository.java
@@ -1,6 +1,9 @@
 package top.felixu.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import top.felixu.entity.Person;
 
 import java.util.List;
@@ -24,4 +27,29 @@ public interface PersonRepository extends JpaRepository<Person, Integer> {
      * @return
      */
     String findByName(String name);
+
+    /**
+     * 查询符合条件的前二十条记录
+     * @param name
+     * @return
+     */
+    List<Person> findFirst20ByName(String name);
+
+    /**
+     * 根据名字和地址查询
+     * @param name
+     * @param address
+     * @return
+     */
+    @Query("select p from Person p where p.name= :name and p.address= :address")
+    Person withNameAndAddressQuery(@Param("name") String name, @Param("address") String address);
+
+    /**
+     * 更新名字
+     * @param name
+     * @return
+     */
+    @Modifying
+    @Query("update Person p set p.name=?1 ")
+    int setName(String name);
 }
