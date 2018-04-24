51b8cbf1f76152a94f0d9a8b695f9ae31b92db7a
felixu <xufei_0320@163.com>
2018-04-24 15:37:44 +0800

    1.0

---
+++ spring-learning/spring1.0/src/main/java/top/felixu/annotation/Autowired.java
@@ -0,0 +1,14 @@
+package top.felixu.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @Author felixu
+ * @Date 2018/4/23
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Autowired {
+    String value() default "";
+}
